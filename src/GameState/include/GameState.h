504a22992b37676fb3b7870a12e5e69d58412528
Michael Torp Kaalund <kaalund@gmail.com>
2019-10-18 11:57:22 +0200

    added get_window_size

--- src/GameState/include/GameState.h
+++ src/GameState/include/GameState.h
@@ -14,10 +14,17 @@ public:
     virtual void update() {};
     virtual void renderer() {};
 
+    void get_window_size() {
+        SDL_GetWindowSize( this->p_window, &p_window_width, &p_window_height );
+    }
+
     bool state_finished;
     bool state_force_quit;
     SDL_Window      * p_window      = nullptr;
     SDL_Renderer    * p_renderer    = nullptr;
+
+    int p_window_width;
+    int p_window_height;
 
     // We need some way of getting a configuration and a save file.
     nlohmann::json    config;
