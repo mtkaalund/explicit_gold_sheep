f0eae5492c2b7fe2875009231afccdf40428585c
Michael Torp Kaalund <kaalund@gmail.com>
2019-10-20 17:08:03 +0200

    created a test state for sandboxed code

--- src/TestState/include/TestState/TestState.h
+++ src/TestState/include/TestState/TestState.h
@@ -1,6 +1,15 @@
 #ifndef TESTSTATE_H_
 #define TESTSTATE_H_
 
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <utility>
+#include <map>
+
 #include <GameState.h>
 
 #include <sdl2core/SDL2.h>
@@ -9,15 +18,23 @@
 #include <sdl2class/Font.h>
 #include <sdl2class/Texture.h>
 
+#include <nlohmann/json.hpp>
+
 namespace mtkaalund {
+
+    typedef std::map<std::string, SDL_Rect> map_of_rect;
+
     class TestState : public mtkaalund::GameState {
         private:
             sdl2class::Texture *    m_image;
             sdl2class::Texture      m_text;
 
             sdl2class::Font         m_font;
-
             int                     m_x_travel;
+            int                     m_x_direction;
+            mtkaalund::map_of_rect  sprite;
+            mtkaalund::map_of_rect::iterator sprite_iterator;
+
         public:
             void init_state();
             void renderer();
--- src/TestState/src/TestState.cc
+++ src/TestState/src/TestState.cc
@@ -3,9 +3,43 @@
 namespace mtkaalund {
 
 void TestState::init_state() {
-    this->m_image = new sdl2class::Texture( this->p_renderer, "res/images/Tux.png" );
+    this->m_image = new sdl2class::Texture( this->p_renderer, "res/UI/Spritesheet/blueSheet.png" );
 
-    if( this->m_font.load( "res/font/waltographUI.ttf", 30) == false ) {
+
+    nlohmann::json spritesheet_json;
+    std::ifstream file_input("res/UI/Spritesheet/blueSheet.json");
+    file_input >> spritesheet_json;
+    file_input.close();
+
+    std::cout << std::setw(4) << spritesheet_json << std::endl;
+
+    std::cout << std::setw(4) << spritesheet_json["TextureAtlas"]["SubTexture"] << std::endl;
+    
+    for(auto& element: spritesheet_json["TextureAtlas"]["SubTexture"]) {
+        std::stringstream str_tmp;
+        
+        str_tmp << std::string(element["@height"]);
+        str_tmp >> sprite[std::string( element["@name"] )].h;
+        
+        str_tmp.clear();
+        str_tmp << std::string(element["@width"]);
+        str_tmp >> sprite[std::string( element["@name"] )].w;
+
+        str_tmp.clear();
+        str_tmp << std::string(element["@x"]);
+        str_tmp >> sprite[std::string( element["@name"] )].x;
+
+        str_tmp.clear();
+        str_tmp << std::string(element["@y"]);
+        str_tmp >> sprite[std::string( element["@name"] )].y;
+    }
+
+    std::cout << "Size of sprite: " << sprite.size() << std::endl;
+
+    sprite_iterator =  sprite.begin();
+    this->m_image->set_clip( &sprite_iterator->second );
+
+    if( this->m_font.load( "res/UI/Font/kenvector_future.ttf", 30) == false ) {
         throw TTFException();
     }
 
@@ -22,6 +56,7 @@ void TestState::init_state() {
 
 
     this->m_x_travel = 0;
+    this->m_x_direction = 1;
 
     this->get_window_size();
 
@@ -32,8 +67,8 @@ void TestState::renderer() {
     SDL_RenderClear( this->p_renderer );
     SDL_SetRenderDrawColor( this->p_renderer, 0xff, 0xff, 0xff, 0xff );
 
-    this->m_image->set_point( this->m_x_travel, p_window_height/2 - this->m_image->get_current_height()/2 );
-    this->m_image->renderer();
+    this->m_image->set_point( this->m_x_travel, p_window_height/2 - sprite_iterator->second.h/2 );
+    this->m_image->renderer( );
 
     this->m_text.set_point( 0, 0 );
     this->m_text.renderer();
@@ -46,20 +81,40 @@ void TestState::handle_event() {
     while( SDL_PollEvent( &event ) ) {
         switch( event.type ) {
             case SDL_QUIT:
+                std::cerr << "Force quit activated!" << std::endl;
                 this->state_force_quit = true;
                 this->state_finished = true;
+                break;
+            case SDL_KEYDOWN:
+                switch( event.key.keysym.sym ) {
+                    case SDLK_ESCAPE:
+                        std::cout << "Soft quit activated" << std::endl;
+                        this->state_finished = true; // Soft quit
+                        break;
+                    case SDLK_SPACE:
+                        this->sprite_iterator++;
+                        if( this->sprite_iterator == sprite.end() ) {
+                            this->sprite_iterator = sprite.begin();
+                        }
+                        this->m_image->set_clip( &sprite_iterator->second );
+
+                        break;
+                }
                 break;
         }
     }
 }
 
 void TestState::update() {
-    this->m_x_travel += 1;
+    this->m_x_travel += this->m_x_direction;
 
     SDL_Delay(10);
 
     if( this->m_x_travel >= this->p_window_width ) {
-        this->state_finished = true;
+        this->m_x_direction = -1;
+        //this->state_finished = true;
+    } else if( this->m_x_travel <= 0 ) {
+        this->m_x_direction = 1;
     }
 }
 
