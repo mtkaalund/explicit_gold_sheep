ea34a3602032340333f1507d46a007dc610d94f0
Michael Torp Kaalund <kaalund@gmail.com>
2019-12-20 10:15:26 +0100

    still working on resourcehandler

---
+++ ResourceHandler/include/ResourceHandler/ResourceHandler.h
@@ -0,0 +1,23 @@
+#ifndef MTKAALUND_RESOURCEHANDLER_H_
+#define MTKAALUND_RESOURCEHANDLER_H_
+//! \file ResourceHandler.h
+//!
+//! 
+namespace mtkaalund {
+
+typedef enum {
+    AUDIO,
+    IMAGE,
+    FONT
+} res_type;
+
+
+
+class ResourceHandler {
+private:
+public:
+};
+
+}
+
+#endif
