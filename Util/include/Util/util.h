99ca92a56ed2048f1489da9002d9c3b343b37d3e
Michael Torp Kaalund <kaalund@gmail.com>
2019-12-27 01:57:46 +0100

    add contains search function for list

--- Util/include/Util/util.h
+++ Util/include/Util/util.h
@@ -6,8 +6,11 @@
 #include <vector>
 
 #include <map>
+#include <list>
 #include <sstream>
 #include <exception>
+#include <algorithm>
+
 #include <sdl2core/SDL2.h>
 #include <sdl2core/IMG.h>
 #include <nlohmann/json.hpp>
@@ -32,6 +35,16 @@ bool create_directory( const std::string& name, bool create_parent);
 typedef std::map<std::string, SDL_Rect> map_of_rect;
 
 map_of_rect load_from_json( std::string filename );
+
+template<typename T>
+bool contains( std::list<T> & listOfElements, const T & element )
+{
+    //Find the iterator if element in list
+    auto it = std::find( listOfElements.begin(), listOfElements.end(), element );
+    // Return if iterator points to end or not. It points to end the it means the element
+    // does not exists in list
+    return it != listOfElements.end();
+}
 
 }
 
